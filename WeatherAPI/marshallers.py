4cd0f5e533c651d842bd3fae1102cec480a2056b
Deepak Kumar <deepak.kumar.ext@deutsche-boerse.com>
2019-12-20 21:10:41 +0100

    added some marshallers

--- WeatherAPI/marshallers.py
+++ WeatherAPI/marshallers.py
@@ -8,4 +8,22 @@ from flask_restful import fields, marshal
 # 4. 'no_temp_fields' :
 # 5. 'preferred_location_details' :
 
+location_details = {
+    'lat': fields.Float,
+    'lon': fields.Float,
+    'city': fields.String,
+    'state': fields.String
+}
 
+resource_fields = {
+    'id': fields.Integer,
+    'date': fields.DateTime(dt_format='iso8601'),
+    'location': fields.Nested(location_details)
+    'temperature': fields.List(fields.Float)
+}
+
+temp_fields = {}
+
+no_temp_fields = {}
+
+preferred_location_details = {}
